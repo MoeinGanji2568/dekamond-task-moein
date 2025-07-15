faaaf8e09723f741aa7c8ca1a8345a41ec701b00
MoeinGanji2568 <moeinganji2004@gmail.com>
2025-07-15 11:32:04 +0330

    Add NotFound component to display 404 error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@/ui/Typography";
+
+const NotFound = () => {
+  return (
+    <div>
+      <Typography variant="h1">404 - Page Not Found</Typography>
+    </div>
+  );
+};
+
+export default NotFound;
